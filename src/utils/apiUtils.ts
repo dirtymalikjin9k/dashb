0451de8e17235625978a9189d0b4558fcdd0c4b5
dirtymalikjin9k <hopemalik0512@gmail.com>
2024-01-13 12:08:31 +0200

    update headers of axios

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -16,7 +16,7 @@ const buildQueryString = (queryObj: any) => (queryObj ? '?' + qs.stringify(query
 
 const axiosApi: AxiosInstance = axios.create({
   headers: {
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'https://dashb-ten.vercel.app/',
     // 'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
     // 'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     // 'Access-Control-Allow-Credentials': 'true'
